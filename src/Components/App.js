0b9c133447db593e8fe90445a250dd57f9a73b18
GiovanniSybot <giovanni.sybot@gmail.com>
2020-11-14 22:08:48 -0300

    .

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import './../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="main">
+          <div class="content">
+              <h2>Olá novamente, Gabi :)</h2>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
